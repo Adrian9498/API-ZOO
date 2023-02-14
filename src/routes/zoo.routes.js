ad0399a197d74c675e4447e22da571980ff19505
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-02-13 21:00:12 -0600

    Mas comentarios a rutas de zoo

--- src/routes/zoo.routes.js
+++ src/routes/zoo.routes.js
@@ -7,10 +7,11 @@ const router = Router();
 //Se agregan funciones desde el controlador
 const {getAllZoo,getOneZoo,getZooBudget,getZooNombre} = require('../controllers/zoo.controller');
 
+//Se definen rutas o enpoints
 router.get('/api/zoo', getAllZoo);
 router.get('/api/zoo/id/:id', getOneZoo);
 router.get('/api/zoo/budget', getZooBudget);
 router.post('/api/zoo/findByNombre', getZooNombre);
 
-
+//Se exportan todos los modulos
 module.exports = router;
