fde5e3b755fe917133c272103ba846b35ee31775
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-02-13 21:18:16 -0600

    insrt de zoo

--- src/routes/zoo.routes.js
+++ src/routes/zoo.routes.js
@@ -5,13 +5,14 @@ const {Router,application} = require('express');
 const router = Router();
 
 //Se agregan funciones desde el controlador
-const {getAllZoo,getOneZoo,getZooBudget,getZooNombre} = require('../controllers/zoo.controller');
+const {getAllZoo,getOneZoo,getZooBudget,getZooNombre,createZoo} = require('../controllers/zoo.controller');
 
 //Se definen rutas o enpoints
 router.get('/api/zoo', getAllZoo);
 router.get('/api/zoo/id/:id', getOneZoo);
 router.get('/api/zoo/budget', getZooBudget);
 router.post('/api/zoo/findByNombre', getZooNombre);
+router.post('/api/zoo/createZoo', createZoo);
 
 //Se exportan todos los modulos
 module.exports = router;
